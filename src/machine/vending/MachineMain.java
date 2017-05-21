f85024e66fe07a3949949ba6d05322e9dea72823
TM004 <s15004@al.kic.ac.jp>
2017-05-21 23:39:07 +0900

    微修正 ver1.01

--- src/machine/vending/MachineMain.java
+++ src/machine/vending/MachineMain.java
@@ -10,28 +10,27 @@ import java.util.Scanner;
  * 自動販売機のお金の計算処理
  */
 public class MachineMain {
+	// check配列は1番目1000円，2番目500円,3番目100円,4番目50円,5番目10円の各ストック数を仮確保する。
 	static int[] check = new int[5];
 	static int number = 0;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int inputMoney = 0;
-		
 		Money ten = new Money(10,5);
 		Money fifty = new Money(50,5);
 		Money hundred = new Money(100,5);
 		Money fiveHundred = new Money(500,5);
 		Money thousand = new Money(1000,5);
-
 		Scanner sc;
 		
-		// check配列は1番目1000円，2番目500円,3番目100円,4番目50円,5番目10円の各ストック数を仮確保する。
-
-
-		// Scanner sc;
 		System.out.println("自動販売機のお金処理です。終了したい時は\"-1\"と入力してください。");
 		while(true){
+			// 配列初期化
+			numberInitializ();
+			
 			// 入力文字が数字かどうかを判定
 			try{
 				// System.out.println("でばっぐー１");
@@ -43,7 +42,6 @@ public class MachineMain {
 					System.out.println("0以上の数字を入力するか、-1を入力して終了してください。");
 					continue;
 				}
-
 				if(inputMoney > 10000){
 					System.out.println("１００００円以上は入れないでください");
 					continue;
@@ -52,11 +50,8 @@ public class MachineMain {
 				System.out.println("数字を入力してください");
 				continue;
 			}
-
 			if(inputMoney == -1) break;
-			// お金のストック処理
 			// check関数完成処理
-			
 			inputMoney = checkCom( inputMoney, thousand.getParametar());
 			// debug(inputMoney);
 			inputMoney = checkCom( inputMoney, fiveHundred.getParametar());
@@ -68,10 +63,10 @@ public class MachineMain {
 			inputMoney = checkCom( inputMoney, ten.getParametar());
 			// System.out.println("inputMoney処理後 : " + inputMoney );
 			
-			// この時点でinputMoneyが0以外だとおかしいため、ブレイク
+			// 割り切れない数字がでたときの処理（現在は1円を入れた時とかの処理を入れていないため）
 			if(inputMoney != 0) {
-				System.out.println("投入金額がおかしいです、アプリを終了します");
-				break;
+				System.out.println("10円50円100円500円1000円を組み合わせた数字を入力してください");
+				continue;
 			}
 			// 配列初期化
 			numberInitializ();
@@ -79,7 +74,6 @@ public class MachineMain {
 			// ストック数確認
 			if( checkStock(thousand.getStock()) & checkStock(fiveHundred.getStock()) & checkStock(hundred.getStock()) & 
 					checkStock(fifty.getStock()) & checkStock(ten.getStock()) ) {
-				numberInitializ();
 				thousand.minusStock( check[0] );
 				fiveHundred.minusStock( check[1] );
 				hundred.minusStock( check[2] );
@@ -88,17 +82,14 @@ public class MachineMain {
 			} else {
 				System.out.println("お金のストック数が足りません！");
 			}
+			
 			//本当に処理ができているかどうかのデバッグ
 			stockDebug(thousand.getParametar(),thousand.getStock());
 			stockDebug(fiveHundred.getParametar(),fiveHundred.getStock());
 			stockDebug(hundred.getParametar(),hundred.getStock());
 			stockDebug(fifty.getParametar(),fifty.getStock());
 			stockDebug(ten.getParametar(),ten.getStock());
-			
-			// 配列初期化
-			numberInitializ();
 		}
-			// 1000円のストック判断
 			sc.close();
 			System.out.println("終了");
 	}
